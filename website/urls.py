146744eb999449bea942ac1e1f36582db39650df
zalduss <seanphillips44@gmail.com>
2024-03-08 13:41:54 -0800

    add form

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .views import home
+from .views import home, logout_user, register_user
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -7,12 +7,30 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # if user is logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
 
-def login_user(request):
-    pass
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have logged in successfully.")
+            return redirect('home')
+        else:
+            messages.success(request, "No account found.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have successfully logged out.")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
