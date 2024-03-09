bc578c7c0437573ba2a9b77f995ac9a7043241bd
zalduss <seanphillips44@gmail.com>
2024-03-08 23:05:45 -0800

    add curd operations

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,14 @@
 
 from django.urls import path
-from .views import home, logout_user, register_user
+from .views import home, logout_user, register_user, customer_record, delete_record, add_record, update_record
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', logout_user, name='logout'),
     path('register/', register_user, name='register'),
+    path('record/<int:pk>', customer_record, name='record'),
+    path('delete_record/<int:pk>', delete_record, name='delete_record'),
+    path('update_record/<int:pk>', update_record, name='update_record'),
+    path('add_record/', add_record, name='add_record')
 
 ]
